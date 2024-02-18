8ba61d936c19aa0f726f85c5937f14f45a6af256
Rumman <rummanfardeen4567@gmail.com>
2024-02-18 21:36:19 +0530

    next config updated  🏗️

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    "baseUrl": ".",
+    paths: {
+        "@/app/*": ["app/*"]
+    }
+};
 
 module.exports = nextConfig;
